cf2f14c3426edbbc87d5cafc7127418b9184027c
Meri Herrera <herrerameri@gmail.com>
2019-06-01 18:00:49 -0300

    Commit inicial - UI

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AFIP
 Proyecto para generación de archivos de carga masiva solicitados por distintos módulos de AFIP Argentina.
+
+## Requerimientos
+- NodeJS > v8.9.0
+- `git clone https://github.com/herrerameri/AFIP.git`
+
+## SIR - Sistema integral de recupero
+TBD
---
+++ sir/app.js
@@ -0,0 +1,74 @@
+var http = require("http");
+var path = require("path");
+var fs = require("fs");
+var CONFIG = require('./config.json');
+
+var checkMimeType = true;
+var port = CONFIG.site.port;
+
+var indexPage = "/index.html";
+
+console.log("Starting web server at http://localhost:" + port);
+
+http.createServer(function (req, res) {
+    loadServerConfiguration(req, res);
+}).listen(port);
+
+function getFile(localPath, res, mimeType) {
+    fs.readFile(localPath, function (err, contents) {
+        if (!err) {
+            res.setHeader("Content-Length", contents.length);
+            if (mimeType != undefined) {
+                res.setHeader("Content-Type", mimeType);
+            }
+            res.statusCode = 200;
+            res.end(contents);
+        } else {
+            res.writeHead(500);
+            res.end();
+        }
+    });
+}
+
+function loadServerConfiguration(req, res) {
+    var filename = req.url == "/" ? indexPage : req.url;
+    var ext = path.extname(filename);
+    var localPath = __dirname;
+    var validExtensions = {
+        ".html": "text/html",
+        ".js": "application/javascript",
+        ".json": "application/json",
+        ".pdf": "application/pdf",
+        ".css": "text/css",
+        ".txt": "text/plain",
+        ".jpg": "image/jpeg",
+        ".ico": "image/ico",
+        ".gif": "image/gif",
+        ".svg": "image/svg+xml",
+        ".png": "image/png",
+        ".woff": "application/font-woff",
+        ".woff2": "application/font-woff2"
+    };
+
+    var validMimeType = true;
+    var mimeType = validExtensions[ext];
+    if (checkMimeType) {
+        validMimeType = validExtensions[ext] != undefined;
+    }
+
+    if (validMimeType) {
+        localPath += filename;
+        fs.exists(localPath, function (exists) {
+            if (exists) {
+                getFile(localPath, res, mimeType);
+            } else {
+                console.log("File not found: " + localPath);
+                res.writeHead(404);
+                res.end();
+            }
+        });
+
+    } else {
+        console.log("Invalid file extension detected: " + ext + " (" + filename + ")")
+    };
+}
